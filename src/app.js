d06b3310f854a4bffc4563e91e1271aae9f7cec6
Nevil Joyice <neviljo38@gmail.com>
2025-02-04 21:51:21 +0530

    util code discussion

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit: "16kb"}));//json accept karna and json ki limit set karna
+app.use(express.urlencoded({extended: true}));//urlencoded data accept karna
+app.use(express.static("public"));//public folder ko static folder me convert kar dena
+app.use(cookieParser());//secure cookies ke liye
+
+export { app };
