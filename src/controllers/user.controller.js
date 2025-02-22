cfb72e295b69425fccce65d692a63b44987d13fd
Nevil Joyice <neviljo38@gmail.com>
2025-02-22 20:20:08 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,14 +20,13 @@ const registerUser = asyncHandler( async (req, res)=>{
 
     const {fullName, email, username, password} = req.body
     console.log("email :",email)
-    if([fullName, email, username, password].some((field)=>field?field.trim() === "")
-    ){
+    if([fullName, email, username, password].some((field)=>field?.trim() === "")){
       throw new ApiError(400, "All fields are required")
 
 
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
       $or: [{username},{email}]
     })
 
@@ -36,7 +35,14 @@ const registerUser = asyncHandler( async (req, res)=>{
     }
 
     const avatarLocalPath =  req.files?.avatar[0]?.path
-    const coverImageLocalPath =  req.files?.coverImage[0]?.path
+    // const coverImageLocalPath =  req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath ;
+
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+      coverImageLocalPath = req.files.coverImage[0].path
+
+    }
 
     if(!avatarLocalPath){
       throw new ApiError(400, " Avatar file  is required")
@@ -51,13 +57,13 @@ const registerUser = asyncHandler( async (req, res)=>{
 
     }
    
-   const user =  User.create({
+   const user = await  User.create({
       fullName,
       avatar: avatar.url,
       coverImage: coverImage?.url||"",
       email,
       password,
-      username: username.toLowercase()
+      username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select(
